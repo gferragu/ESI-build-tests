6aaa0179cf773eada8daffe2e5535662881cc88c
Gabe Ferragut <gferragut@usgs.gov>
2023-01-18 09:39:01 -0700

    Azure testing

--- tests/gmprocess/bin/gmconvert_test.py
+++ tests/gmprocess/bin/gmconvert_test.py
@@ -12,6 +12,8 @@ from gmprocess.utils.constants import TEST_DATA_DIR
 def test_gmconvert():
     print("The variable TEST_DATA_DIR is currently: " + str(TEST_DATA_DIR))
     print("__file__ is: " + __file__)
+    print("pathlib.Path(__file__).parent is: " + pathlib.Path(__file__).parent)
+
     data_dir = TEST_DATA_DIR / "demo" / "ci38457511" / "raw"
     out_dir = "temp_dir"
     try:
